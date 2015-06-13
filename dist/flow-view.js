c3f1dc992fa13a1dd6db6ea007aae031642a4820
fibo <casati_gianluca@yahoo.it>
2015-06-13 18:54:31 -0400

    updated

--- dist/flow-view.js
+++ dist/flow-view.js
@@ -5066,8 +5066,16 @@ module.exports = theme
 var Canvas = require('./Canvas')
 exports.Canvas = Canvas
 
-function render (element) {
+function render (element, callback) {
   return function loading (graph) {
+    try {
+    if (typeof callback === 'function')
+      callback(graph)
+    }
+    catch (message) {
+      console.error(message)
+    }
+
     return new Canvas(element, graph.view)
   }
 }

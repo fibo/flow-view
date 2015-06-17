3d2a94dac2bc68939f43337fe6ab24fb945fac17
fibo <casati_gianluca@yahoo.it>
2015-06-17 04:53:16 -0500

    fixed build

--- gh-pages/dist/flow-view.js
+++ gh-pages/dist/flow-view.js
@@ -1,4 +1,332 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+function EventEmitter() {
+  this._events = this._events || {};
+  this._maxListeners = this._maxListeners || undefined;
+}
+module.exports = EventEmitter;
+
+// Backwards-compat with node 0.10.x
+EventEmitter.EventEmitter = EventEmitter;
+
+EventEmitter.prototype._events = undefined;
+EventEmitter.prototype._maxListeners = undefined;
+
+// By default EventEmitters will print a warning if more than 10 listeners are
+// added to it. This is a useful default which helps finding memory leaks.
+EventEmitter.defaultMaxListeners = 10;
+
+// Obviously not all Emitters should be limited to 10. This function allows
+// that to be increased. Set to zero for unlimited.
+EventEmitter.prototype.setMaxListeners = function(n) {
+  if (!isNumber(n) || n < 0 || isNaN(n))
+    throw TypeError('n must be a positive number');
+  this._maxListeners = n;
+  return this;
+};
+
+EventEmitter.prototype.emit = function(type) {
+  var er, handler, len, args, i, listeners;
+
+  if (!this._events)
+    this._events = {};
+
+  // If there is no 'error' event listener then throw.
+  if (type === 'error') {
+    if (!this._events.error ||
+        (isObject(this._events.error) && !this._events.error.length)) {
+      er = arguments[1];
+      if (er instanceof Error) {
+        throw er; // Unhandled 'error' event
+      }
+      throw TypeError('Uncaught, unspecified "error" event.');
+    }
+  }
+
+  handler = this._events[type];
+
+  if (isUndefined(handler))
+    return false;
+
+  if (isFunction(handler)) {
+    switch (arguments.length) {
+      // fast cases
+      case 1:
+        handler.call(this);
+        break;
+      case 2:
+        handler.call(this, arguments[1]);
+        break;
+      case 3:
+        handler.call(this, arguments[1], arguments[2]);
+        break;
+      // slower
+      default:
+        len = arguments.length;
+        args = new Array(len - 1);
+        for (i = 1; i < len; i++)
+          args[i - 1] = arguments[i];
+        handler.apply(this, args);
+    }
+  } else if (isObject(handler)) {
+    len = arguments.length;
+    args = new Array(len - 1);
+    for (i = 1; i < len; i++)
+      args[i - 1] = arguments[i];
+
+    listeners = handler.slice();
+    len = listeners.length;
+    for (i = 0; i < len; i++)
+      listeners[i].apply(this, args);
+  }
+
+  return true;
+};
+
+EventEmitter.prototype.addListener = function(type, listener) {
+  var m;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events)
+    this._events = {};
+
+  // To avoid recursion in the case that type === "newListener"! Before
+  // adding it to the listeners, first emit "newListener".
+  if (this._events.newListener)
+    this.emit('newListener', type,
+              isFunction(listener.listener) ?
+              listener.listener : listener);
+
+  if (!this._events[type])
+    // Optimize the case of one listener. Don't need the extra array object.
+    this._events[type] = listener;
+  else if (isObject(this._events[type]))
+    // If we've already got an array, just append.
+    this._events[type].push(listener);
+  else
+    // Adding the second element, need to change to array.
+    this._events[type] = [this._events[type], listener];
+
+  // Check for listener leak
+  if (isObject(this._events[type]) && !this._events[type].warned) {
+    var m;
+    if (!isUndefined(this._maxListeners)) {
+      m = this._maxListeners;
+    } else {
+      m = EventEmitter.defaultMaxListeners;
+    }
+
+    if (m && m > 0 && this._events[type].length > m) {
+      this._events[type].warned = true;
+      console.error('(node) warning: possible EventEmitter memory ' +
+                    'leak detected. %d listeners added. ' +
+                    'Use emitter.setMaxListeners() to increase limit.',
+                    this._events[type].length);
+      if (typeof console.trace === 'function') {
+        // not supported in IE 10
+        console.trace();
+      }
+    }
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+EventEmitter.prototype.once = function(type, listener) {
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  var fired = false;
+
+  function g() {
+    this.removeListener(type, g);
+
+    if (!fired) {
+      fired = true;
+      listener.apply(this, arguments);
+    }
+  }
+
+  g.listener = listener;
+  this.on(type, g);
+
+  return this;
+};
+
+// emits a 'removeListener' event iff the listener was removed
+EventEmitter.prototype.removeListener = function(type, listener) {
+  var list, position, length, i;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events || !this._events[type])
+    return this;
+
+  list = this._events[type];
+  length = list.length;
+  position = -1;
+
+  if (list === listener ||
+      (isFunction(list.listener) && list.listener === listener)) {
+    delete this._events[type];
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+
+  } else if (isObject(list)) {
+    for (i = length; i-- > 0;) {
+      if (list[i] === listener ||
+          (list[i].listener && list[i].listener === listener)) {
+        position = i;
+        break;
+      }
+    }
+
+    if (position < 0)
+      return this;
+
+    if (list.length === 1) {
+      list.length = 0;
+      delete this._events[type];
+    } else {
+      list.splice(position, 1);
+    }
+
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.removeAllListeners = function(type) {
+  var key, listeners;
+
+  if (!this._events)
+    return this;
+
+  // not listening for removeListener, no need to emit
+  if (!this._events.removeListener) {
+    if (arguments.length === 0)
+      this._events = {};
+    else if (this._events[type])
+      delete this._events[type];
+    return this;
+  }
+
+  // emit removeListener for all listeners on all events
+  if (arguments.length === 0) {
+    for (key in this._events) {
+      if (key === 'removeListener') continue;
+      this.removeAllListeners(key);
+    }
+    this.removeAllListeners('removeListener');
+    this._events = {};
+    return this;
+  }
+
+  listeners = this._events[type];
+
+  if (isFunction(listeners)) {
+    this.removeListener(type, listeners);
+  } else {
+    // LIFO order
+    while (listeners.length)
+      this.removeListener(type, listeners[listeners.length - 1]);
+  }
+  delete this._events[type];
+
+  return this;
+};
+
+EventEmitter.prototype.listeners = function(type) {
+  var ret;
+  if (!this._events || !this._events[type])
+    ret = [];
+  else if (isFunction(this._events[type]))
+    ret = [this._events[type]];
+  else
+    ret = this._events[type].slice();
+  return ret;
+};
+
+EventEmitter.listenerCount = function(emitter, type) {
+  var ret;
+  if (!emitter._events || !emitter._events[type])
+    ret = 0;
+  else if (isFunction(emitter._events[type]))
+    ret = 1;
+  else
+    ret = emitter._events[type].length;
+  return ret;
+};
+
+function isFunction(arg) {
+  return typeof arg === 'function';
+}
+
+function isNumber(arg) {
+  return typeof arg === 'number';
+}
+
+function isObject(arg) {
+  return typeof arg === 'object' && arg !== null;
+}
+
+function isUndefined(arg) {
+  return arg === void 0;
+}
+
+},{}],2:[function(require,module,exports){
+if (typeof Object.create === 'function') {
+  // implementation from standard node.js 'util' module
+  module.exports = function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  };
+} else {
+  // old school shim for old browsers
+  module.exports = function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor
+    var TempCtor = function () {}
+    TempCtor.prototype = superCtor.prototype
+    ctor.prototype = new TempCtor()
+    ctor.prototype.constructor = ctor
+  }
+}
+
+},{}],3:[function(require,module,exports){
 /*! svg.draggable.js - v1.0.0 - 2015-06-12
 * https://github.com/wout/svg.draggable.js
 * Copyright (c) 2015 Wout Fierens; Licensed MIT */
@@ -190,7 +518,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
   })
 
 }).call(this);
-},{}],2:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 /*!
 * SVG.js - A lightweight library for manipulating and animating SVG.
 * @version 2.0.0-rc.2
@@ -4496,7 +4824,7 @@ return SVG;
 
 }));
 
-},{}],3:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 
 var Input   = require('./Input'),
     Output  = require('./Output')
@@ -4519,7 +4847,7 @@ function Box (canvas, view, key) {
     view.h = 1
 
   if (typeof view.w === 'undefined')
-    view.w = view.text.length
+    view.w = view.text.length + 2
 
   var h = view.h * theme.unitHeight,
       w = view.w * theme.unitWidth
@@ -4570,26 +4898,16 @@ function Box (canvas, view, key) {
       Object.keys(output.link).forEach(function (key) {
         var link = output.link[key]
 
-        var x1 = link.x1,
-            y1 = link.y1,
-            x2 = link.x2,
-            y2 = link.y2
-
-        link.line.plot(x1, y1, x2, y2)
+        if (link)
+          link.linePlot()
       })
     })
 
     this.ins.forEach(function (input) {
       var link = input.link
 
-      if (!link) return
-
-      var x1 = link.x1
-        , y1 = link.y1
-        , x2 = link.x2
-        , y2 = link.y2
-
-      link.line.plot(x1, y1, x2, y2)
+      if (link)
+        link.linePlot()
     })
   }
 
@@ -4601,7 +4919,9 @@ function Box (canvas, view, key) {
       y: this.y,
       w: this.w,
       h: this.h,
-      text: this.text
+      text: this.text,
+      ins: this.ins,
+      outs: this.outs
     }
 
     return view
@@ -4613,19 +4933,35 @@ function Box (canvas, view, key) {
 module.exports = Box
 
 
-},{"./Input":5,"./Output":7}],4:[function(require,module,exports){
+},{"./Input":8,"./Output":10}],6:[function(require,module,exports){
 
-var SVG = require('./SVG')
+function BoxSelector (canvas) {
+  this.draw = draw
 
-var Box  = require('./Box'),
-    Link = require('./Link')
+}
+
+function showBoxSelector () {
+  console.log('show box selector')
+}
+
+BoxSelector.prototype.show = showBoxSelector
+
+module.exports = BoxSelector
+
+
+},{}],7:[function(require,module,exports){
+
+var EventEmitter = require('events').EventEmitter,
+    inherits     = require('inherits'),
+    SVG          = require('./SVG')
+
+var Box         = require('./Box'),
+    BoxSelector = require('./BoxSelector'),
+    Link        = require('./Link')
 
 var defaultTheme = require('./Theme')
 
-var defaultView = {
-  box: {},
-  link: {}
-}
+var defaultView = { box: {}, link: {} }
 
 function Canvas (id, view, theme) {
   this.view  = view  || defaultView
@@ -4633,6 +4969,13 @@ function Canvas (id, view, theme) {
 
   var box  = this.box  = {}
   var link = this.link = {}
+
+  var boxSelector = new BoxSelector(canvas)
+  this.boxSelector = boxSelector
+
+  var element = document.getElementById(id)
+
+  SVG.on(element, 'click', boxSelector.show.bind(boxSelector))
 
   var draw = this.draw = SVG(id).size(1000, 1000)
                                 .spof()
@@ -4643,7 +4986,8 @@ function Canvas (id, view, theme) {
     this.addBox(view, key)
   }
 
-  Object.keys(view.box).forEach(createBox.bind(this))
+  Object.keys(view.box)
+        .forEach(createBox.bind(this))
 
   function createLink (key) {
     var view = this.view.link[key]
@@ -4671,6 +5015,8 @@ function Canvas (id, view, theme) {
   Object.defineProperty(this, 'nextKey', { get: getNextKey })
 }
 
+inherits(Canvas, EventEmitter)
+
 function addBox (view, key) {
   if (typeof key === 'undefined')
      key = this.nextKey
@@ -4692,11 +5038,15 @@ Canvas.prototype.addLink = addLink
 module.exports = Canvas
 
 
-},{"./Box":3,"./Link":6,"./SVG":9,"./Theme":10}],5:[function(require,module,exports){
+},{"./Box":5,"./BoxSelector":6,"./Link":9,"./SVG":12,"./Theme":13,"events":1,"inherits":2}],8:[function(require,module,exports){
 
 function Input (box, position, numIns) {
   this.box      = box
   this.position = position
+
+  function getData () { return box.ins[position] }
+
+  Object.defineProperty(this, 'data', { get: getData })
 
   this.link = null
 
@@ -4762,7 +5112,7 @@ function Input (box, position, numIns) {
 module.exports = Input
 
 
-},{}],6:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 
 function Link (canvas, view, key) {
   var draw = canvas.draw
@@ -4810,16 +5160,32 @@ function Link (canvas, view, key) {
   line.on('mouseout', deselectLine)
 }
 
+function linePlot () {
+var line = this.line,
+    x1   = this.x1,
+    y1   = this.y1,
+    x2   = this.x2,
+    y2   = this.y2
+
+  line.plot(x1, y1, x2, y2)
+}
+
+Link.prototype.linePlot
+
 module.exports = Link
 
 
-},{}],7:[function(require,module,exports){
+},{}],10:[function(require,module,exports){
 
 var PreLink = require('./PreLink')
 
 function Output (box, position, numOuts) {
   this.box      = box
   this.position = position
+
+  function getData () { return box.outs[position] }
+
+  Object.defineProperty(this, 'data', { get: getData })
 
   this.link = {}
 
@@ -4892,7 +5258,7 @@ function Output (box, position, numOuts) {
 module.exports = Output
 
 
-},{"./PreLink":8}],8:[function(require,module,exports){
+},{"./PreLink":11}],11:[function(require,module,exports){
 
 var Link = require('./Link')
 
@@ -5032,7 +5398,7 @@ module.exports = PreLink
 
 
 
-},{"./Link":6}],9:[function(require,module,exports){
+},{"./Link":9}],12:[function(require,module,exports){
 
 // Consider this module will be browserified.
 //
@@ -5046,7 +5412,7 @@ require('svg.draggable.js')
 module.exports = SVG
 
 
-},{"svg.draggable.js":1,"svg.js":2}],10:[function(require,module,exports){
+},{"svg.draggable.js":3,"svg.js":4}],13:[function(require,module,exports){
 
 var theme = {
   unitHeight: 40,
@@ -5070,7 +5436,7 @@ var theme = {
 module.exports = theme
 
 
-},{}],11:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
 
 var Canvas = require('./Canvas')
 exports.Canvas = Canvas
@@ -5092,9 +5458,9 @@ function render (element, callback) {
 exports.render = render
 
 
-},{"./Canvas":4}],"flow-view":[function(require,module,exports){
+},{"./Canvas":7}],"flow-view":[function(require,module,exports){
 
 module.exports = require('./src')
 
 
-},{"./src":11}]},{},[]);
+},{"./src":14}]},{},[]);

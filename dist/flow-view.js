d87fe21b39570c24ec5b407f4a244d32bb6e37ff
fibo <casati_gianluca@yahoo.it>
2015-06-18 15:23:18 -0400

    fixed foreignObject reference

--- dist/flow-view.js
+++ dist/flow-view.js
@@ -4976,13 +4976,14 @@ function BoxSelector (canvas) {
                           .attr({id: 'flow-view-box-selector'})
 
   var txt = "this is a box selector"
-  var fobj = foreignobject
-      fobj.appendChild("div", {id: 'mydiv', innerText: txt})
-      var n = fobj.getChild(0)
-      fobj.attr({width: 200, height: 100}).rotate(45).move(100,0)
-      n.style.height = '50px'
-      n.style.overflow = 'hidden'
-      n.style.border = "solid black 1px"
+  foreignObject.appendChild("div", {id: 'mydiv', innerText: txt})
+
+  var n = foreignObject.getChild(0)
+  foreignObject.attr({width: 200, height: 100}).rotate(45).move(100,0)
+
+  n.style.height = '50px'
+  n.style.overflow = 'hidden'
+  n.style.border = "solid black 1px"
 }
 
 function showBoxSelector () {

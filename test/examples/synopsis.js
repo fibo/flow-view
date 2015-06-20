2fc2dccaf867ecb3d24d7b8b6751537a773fe72f
fibo <casati_gianluca@yahoo.it>
2015-06-20 11:21:37 -0400

    added travis badge

--- test/examples/synopsis.js
+++ test/examples/synopsis.js
@@ -4,6 +4,8 @@ var fs     = require('fs'),
     should = require('should')
 
 describe('examples/synopsis.html', function () {
+  var context
+
   before(function (next) {
     var dist     = fs.readFileSync('./dist/flow-view.js', 'utf-8'),
         synopsis = fs.readFileSync('./gh-pages/js/synopsis.js', 'utf-8')
@@ -11,33 +13,26 @@ describe('examples/synopsis.html', function () {
     jsdom.env({
       url: 'http://g14n.info/flow-view/examples/synopsis.html',
       scripts: [
-      /*
-        dist,
-        synopsis
-        'http://g14n.info/flow-view/dist/flow-view.js',
-        'http://g14n.info/flow-view/js/synopsis.js',
-        */
+        'http://g14n.info/flow-view/dist/flow-view.js'/*,
+        'http://g14n.info/flow-view/js/synopsis.js'*/
       ],
       done: function (errors, window) {
-                //console.log(window.getElementById('drawing'))
+              context = window
               if (errors) {
                 console.log(errors)
                 var err = new Error(errors[0].data.error)
                 next(err)
               }
               else next()
-                /*
-                if (typeof errors === 'undefined') {
-                  next()
-                }
-                else {
-                }
-                */
             }
       })
     })
 
   it('works', function (next) {
+    var document = context.document
+//    console.log(context.require('flow-view'))
+//    console.log(context.require('flow-view'))
+//    console.log(document.getElementById('drawing').childNodes)
     next()
   })
 })

dc8bbe26ffba683f43ac0f8fadc9362a7e732ea7
fibo <casati_gianluca@yahoo.it>
2016-05-05 15:29:14 -0400

    using react

---
+++ test/components/Canvas.js
@@ -0,0 +1,21 @@
+import dom from 'cheerio'
+import React from 'react'
+import reactDom from 'react-dom/server'
+import Canvas from 'components/Canvas'
+import test from 'tape'
+
+const render = reactDom.renderToStaticMarkup
+
+test('Canvas', (t) => {
+  const height = 100
+  const width = 100
+
+  const el = <Canvas width={width} height={height}/>
+
+  const $ = dom.load(render(el), { xmlMode: true })
+
+  t.equal($('svg').attr('width'), width.toString(), 'width prop')
+  t.equal($('svg').attr('height'), height.toString(), 'height prop')
+
+  t.end()
+})

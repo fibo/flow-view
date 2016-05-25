dcb34f19888f8cbe680c63a31ba565e25fbf0188
fibo <casati_gianluca@yahoo.it>
2016-05-25 05:25:07 -0400

    removed model

--- src/model/enumerableProps.js
+++
@@ -1,9 +0,0 @@
-import staticProps from 'static-props'
-
-const enumerable = true
-
-function enumerableProps (obj, props) {
-  staticProps(obj)(props, enumerable)
-}
-
-export default enumerableProps
--- src/model/index.js
+++
@@ -1,6 +0,0 @@
-import Canvas from './Canvas'
-import Item from './Item'
-import Link from './Link'
-import Node from './Node'
-
-export default { Canvas, Item, Link, Node }

35ea6f4d744c6a14e93083d1090471f7615a184c
fibo <casati_gianluca@yahoo.it>
2016-06-06 10:12:57 +0200

    moving initialState to util/

--- src/validate.js
+++
@@ -1,9 +0,0 @@
-function validate (view) {
-  if (typeof view !== 'object') throw new TypeError()
-
-  if (typeof view.node !== 'object') throw new TypeError()
-
-  if (typeof view.link !== 'object') throw new TypeError()
-}
-
-module.exports = validate

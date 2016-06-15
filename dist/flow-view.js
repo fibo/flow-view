28172986ed51c2efc67f81df130f4ccef8c1426d
fibo <casati_gianluca@yahoo.it>
2016-06-15 16:48:46 -0400

    updated build

--- dist/flow-view.js
+++ dist/flow-view.js
@@ -313,8 +313,6 @@ var Canvas = function Canvas(_ref) {
         delNode: delNode(node.id),
         endDragging: endDraggingItems,
         isDraggingLink: isDraggingLink,
-        endDraggingLink: endDraggingLink,
-        draggedLinkId: draggedLinkId,
         setNumIns: setNumIns(node.id),
         setNumOuts: setNumOuts(node.id)
       }, node));
@@ -553,8 +551,6 @@ var Node = function Node(_ref) {
   var selected = _ref.selected;
   var setNumIns = _ref.setNumIns;
   var setNumOuts = _ref.setNumOuts;
-  var endDragging = _ref.endDragging;
-  var endDraggingLink = _ref.endDraggingLink;
   return _react2.default.createElement(
     'g',
     {
@@ -619,7 +615,6 @@ var Node = function Node(_ref) {
         onClick: _ignoreEvent2.default,
         onMouseLeave: _ignoreEvent2.default,
         onMouseDown: _ignoreEvent2.default,
-        onMouseUp: isDraggingLink ? endDraggingLink(draggedLinkId, { to: [id, i] }) : undefined,
         style: styles.defaultPin
       });
     }),
@@ -650,7 +645,6 @@ Node.propTypes = {
   ins: _react.PropTypes.array.isRequired,
   outs: _react.PropTypes.array.isRequired,
   selectNode: _react.PropTypes.func.isRequired,
-  endDragging: _react.PropTypes.func.isRequired,
   selected: _react.PropTypes.bool.isRequired,
   addLink: _react.PropTypes.func.isRequired
 };
@@ -738,20 +732,21 @@ var _Canvas2 = _interopRequireDefault(_Canvas);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var pinRadius = 6;
-var fontWidth = 8;
-var nodeHeight = 40;
-
 var mapStateToProps = function mapStateToProps(state, ownProps) {
   var nodes = [];
 
   var draggedLinkId = state.draggedLinkId;
+
+  var pinRadius = state.pinRadius;
+  var nodeHeight = state.nodeHeight;
+  var fontWidth = state.fontWidth;
 
   var previousDraggingPoint = state.previousDraggingPoint;
 
   var _loop = function _loop(id) {
     var node = Object.assign({}, { ins: [], outs: [] }, state.node[id]);
 
+    // TODO these two lines are repeated in reducers/index.js, refactor them!
     var height = node.height || nodeHeight;
     var width = node.width || (node.text.length + 4) * fontWidth;
 
@@ -942,7 +937,7 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch, ownProps) {
         dispatch((0, _actions.dragItems)(previousDraggingPoint, draggingDelta));
       };
     },
-    endDraggingLink: function endDraggingLink(draggedLinkId, link) {
+    endDraggingLink: function endDraggingLink(draggedLinkId) {
       return function (e) {
         e.preventDefault();
         e.stopPropagation();
@@ -22206,9 +22201,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _initialState = require('../util/initialState');
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-var _initialState2 = _interopRequireDefault(_initialState);
+var _xCenterOfPin = require('../util/xCenterOfPin');
+
+var _xCenterOfPin2 = _interopRequireDefault(_xCenterOfPin);
 
 var _actions = require('../actions');
 
@@ -22223,7 +22220,7 @@ var _setNumOuts2 = _interopRequireDefault(_setNumOuts);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var flowViewApp = function flowViewApp() {
-  var state = arguments.length <= 0 || arguments[0] === undefined ? _initialState2.default : arguments[0];
+  var state = arguments.length <= 0 || arguments[0] === undefined ? initialState : arguments[0];
   var action = arguments[1];
 
   var nextId = 0;
@@ -22242,161 +22239,220 @@ var flowViewApp = function flowViewApp() {
     if (foundId) return generateId();else return newId;
   };
 
-  switch (action.type) {
-    case _actions.ADD_LINK:
-      var link = Object.assign({}, state.link);
+  var _ret = function () {
+    switch (action.type) {
+      case _actions.ADD_LINK:
+        var link = Object.assign({}, state.link);
 
-      link[generateId()] = action.link;
+        link[generateId()] = action.link;
 
-      return Object.assign({}, state, {
-        link: link,
-        selectedItems: [],
-        previousDraggingPoint: action.previousDraggingPoint
-      });
+        return {
+          v: Object.assign({}, state, {
+            link: link,
+            selectedItems: [],
+            previousDraggingPoint: action.previousDraggingPoint
+          })
+        };
 
-    case _actions.ADD_NODE:
-      var node = Object.assign({}, state.node);
+      case _actions.ADD_NODE:
+        var node = Object.assign({}, state.node);
 
-      node[generateId()] = action.node;
+        node[generateId()] = action.node;
 
-      return Object.assign({}, state, {
-        node: node,
-        nodeSelector: null
-      });
+        return {
+          v: Object.assign({}, state, {
+            node: node,
+            nodeSelector: null
+          })
+        };
 
-    case _actions.DEL_NODE:
-      var nextState = Object.assign({}, state);
+      case _actions.DEL_NODE:
+        var nextState = Object.assign({}, state);
 
-      var nodeid = action.id;
+        var nodeid = action.id;
 
-      delete nextState.node[nodeid];
+        delete nextState.node[nodeid];
 
-      for (var linkid in nextState.link) {
-        var isSource = nextState.link[linkid].from[0] === nodeid;
-        var isTarget = nextState.link[linkid].to[0] === nodeid;
+        for (var linkid in nextState.link) {
+          var isSource = nextState.link[linkid].from[0] === nodeid;
+          var isTarget = nextState.link[linkid].to[0] === nodeid;
 
-        if (isSource || isTarget) delete nextState.link[linkid];
-      }
+          if (isSource || isTarget) delete nextState.link[linkid];
+        }
 
-      return nextState;
+        return {
+          v: nextState
+        };
 
-    case _actions.DRAG_ITEMS:
-      var dx = action.draggingDelta.x;
-      var dy = action.draggingDelta.y;
+      case _actions.DRAG_ITEMS:
+        var dx = action.draggingDelta.x;
+        var dy = action.draggingDelta.y;
 
-      var draggedNodes = Object.assign({}, state.node);
-      var previousDraggingPoint = {
-        x: state.previousDraggingPoint.x + dx,
-        y: state.previousDraggingPoint.y + dy
-      };
+        var draggedNodes = Object.assign({}, state.node);
+        var previousDraggingPoint = {
+          x: state.previousDraggingPoint.x + dx,
+          y: state.previousDraggingPoint.y + dy
+        };
 
-      for (var _nodeid in draggedNodes) {
-        var isDraggedNode = state.selectedItems.indexOf(_nodeid) === -1;
+        for (var _nodeid in draggedNodes) {
+          var isDraggedNode = state.selectedItems.indexOf(_nodeid) === -1;
 
-        if (isDraggedNode) {
-          continue;
+          if (isDraggedNode) {
+            continue;
+          } else {
+            draggedNodes[_nodeid].x += dx;
+            draggedNodes[_nodeid].y += dy;
+          }
+        }
+
+        return {
+          v: Object.assign({}, state, {
+            node: draggedNodes,
+            previousDraggingPoint: previousDraggingPoint
+          })
+        };
+
+      case _actions.DRAG_LINK:
+        return {
+          v: Object.assign({}, state, {
+            previousDraggingPoint: {
+              x: state.previousDraggingPoint.x + action.draggingDelta.x,
+              y: state.previousDraggingPoint.y + action.draggingDelta.y
+            }
+          })
+        };
+
+      case _actions.END_DRAGGING_ITEMS:
+        return {
+          v: Object.assign({}, state, {
+            isDraggingItems: false,
+            previousDraggingPoint: null
+          })
+        };
+
+      case _actions.END_DRAGGING_LINK:
+        var newLink = Object.assign({}, state.link);
+        var lastX = state.previousDraggingPoint.x;
+        var lastY = state.previousDraggingPoint.y;
+        var linkId = action.id;
+        var draggedLink = Object.assign({}, state.link[linkId]);
+
+        var pinRadius = state.pinRadius;
+        var nodeHeight = state.nodeHeight;
+        var fontWidth = state.fontWidth;
+
+        var to = draggedLink.to;
+        var from = draggedLink.from;
+
+        Object.keys(state.node).forEach(function (nodeid) {
+          // Cannot connect a node to itself.
+          if (from[0] === nodeid) return;
+
+          var node = state.node[nodeid];
+
+          var height = node.height || nodeHeight;
+          var width = node.width || (node.text.length + 4) * fontWidth;
+
+          // Nothing to do if drag ends outside the node.
+          if (lastX < node.x || lastX > node.x + width) return;
+          if (lastY < node.y || lastY > node.y + height) return;
+
+          node.ins.forEach(function (pin, i) {
+            var cx = node.x + (0, _xCenterOfPin2.default)(pinRadius, width, node.ins.length, i);
+            var cy = node.y + pinRadius;
+            var r = 2 * pinRadius;
+
+            if (lastX > cx - r && lastX < cx + r) to = [nodeid, i];
+          });
+        });
+
+        // Connect link to target...
+        if (to) newLink[linkId].to = to;
+        // ...or remove dragged link if no target was found.
+        else delete newLink[linkId];
+
+        return {
+          v: Object.assign({}, state, {
+            previousDraggingPoint: null,
+            draggedLinkId: null,
+            link: newLink
+          })
+        };
+
+      case _actions.HIDE_NODE_SELECTOR:
+        return {
+          v: Object.assign({}, state, {
+            nodeSelector: null
+          })
+        };
+
+      case _actions.SELECT_ITEM:
+        var selectedItems = [];
+        var itemId = action.id;
+
+        var indexOfSelectedItem = selectedItems.indexOf(itemId);
+        var itemIsNotAlreadySelected = indexOfSelectedItem === -1;
+
+        if (itemIsNotAlreadySelected) {
+          selectedItems.push(itemId);
         } else {
-          draggedNodes[_nodeid].x += dx;
-          draggedNodes[_nodeid].y += dy;
+          selectedItems.splice(indexOfSelectedItem, 1);
         }
-      }
 
-      return Object.assign({}, state, {
-        node: draggedNodes,
-        previousDraggingPoint: previousDraggingPoint
-      });
+        return {
+          v: Object.assign({}, state, {
+            isDraggingItems: true,
+            previousDraggingPoint: {
+              x: action.x,
+              y: action.y
+            },
+            selectedItems: selectedItems,
+            nodeSelector: null
+          })
+        };
 
-    case _actions.DRAG_LINK:
-      return Object.assign({}, state, {
-        previousDraggingPoint: {
-          x: state.previousDraggingPoint.x + action.draggingDelta.x,
-          y: state.previousDraggingPoint.y + action.draggingDelta.y
-        }
-      });
+      case _actions.SET_NODE_SELECTOR_TEXT:
+        return {
+          v: Object.assign({}, state, {
+            nodeSelector: {
+              text: action.text
+            }
+          })
+        };
 
-    case _actions.END_DRAGGING_ITEMS:
-      return Object.assign({}, state, {
-        isDraggingItems: false,
-        previousDraggingPoint: null
-      });
+      case _actions.SET_NUM_INS:
+        return {
+          v: (0, _setNumIns2.default)(state, action)
+        };
 
-    case _actions.END_DRAGGING_LINK:
-      var newLink = Object.assign({}, state.link);
+      case _actions.SET_NUM_OUTS:
+        return {
+          v: (0, _setNumOuts2.default)(state, action)
+        };
 
-      if (action.link) {
-        var newFrom = action.link.from;
-        var newTo = action.link.to;
+      case _actions.SHOW_NODE_SELECTOR:
+        return {
+          v: Object.assign({}, state, {
+            nodeSelector: {
+              x: action.x,
+              y: action.y
+            }
+          })
+        };
 
-        if (newFrom) newLink[action.id].from = newFrom;
-        if (newTo) newLink[action.id].to = newTo;
-      } else {
-        // Remove dragged link.
-        delete newLink[action.id];
-      }
+      default:
+        return {
+          v: state
+        };
+    }
+  }();
 
-      return Object.assign({}, state, {
-        previousDraggingPoint: null,
-        draggedLinkId: null,
-        link: newLink
-      });
-
-    case _actions.HIDE_NODE_SELECTOR:
-      return Object.assign({}, state, {
-        nodeSelector: null
-      });
-
-    case _actions.SELECT_ITEM:
-      var selectedItems = [];
-      var itemId = action.id;
-
-      var indexOfSelectedItem = selectedItems.indexOf(itemId);
-      var itemIsNotAlreadySelected = indexOfSelectedItem === -1;
-
-      if (itemIsNotAlreadySelected) {
-        selectedItems.push(itemId);
-      } else {
-        selectedItems.splice(indexOfSelectedItem, 1);
-      }
-
-      return Object.assign({}, state, {
-        isDraggingItems: true,
-        previousDraggingPoint: {
-          x: action.x,
-          y: action.y
-        },
-        selectedItems: selectedItems,
-        nodeSelector: null
-      });
-
-    case _actions.SET_NODE_SELECTOR_TEXT:
-      return Object.assign({}, state, {
-        nodeSelector: {
-          text: action.text
-        }
-      });
-
-    case _actions.SET_NUM_INS:
-      return (0, _setNumIns2.default)(state, action);
-
-    case _actions.SET_NUM_OUTS:
-      return (0, _setNumOuts2.default)(state, action);
-
-    case _actions.SHOW_NODE_SELECTOR:
-      return Object.assign({}, state, {
-        nodeSelector: {
-          x: action.x,
-          y: action.y
-        }
-      });
-
-    default:
-      return state;
-  }
+  if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
 };
 
 exports.default = flowViewApp;
 
-},{"../actions":2,"../util/initialState":205,"./setNumIns":201,"./setNumOuts":202}],201:[function(require,module,exports){
+},{"../actions":2,"../util/xCenterOfPin":206,"./setNumIns":201,"./setNumOuts":202}],201:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -22542,7 +22598,10 @@ var initialState = {
   isDraggingItems: false,
   previousDraggingPoint: null,
   nodeSelector: null,
-  draggedLinkId: null
+  draggedLinkId: null,
+  pinRadius: 6,
+  nodeHeight: 40,
+  fontWidth: 8
 };
 
 exports.default = initialState;

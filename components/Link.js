db128d6f76e12b74d0820b8ab4b390d07652e388
fibo <casati_gianluca@yahoo.it>
2016-06-16 14:04:51 +0200

    added livereactload :metal:

--- components/Link.js
+++ components/Link.js
@@ -3,7 +3,6 @@ import React, { PropTypes } from 'react'
 const stroke = '#333333'
 const strokeWidth = 3
 const highlightedStrokeWidth = 4
-const strokeDasharray = '5, 5'
 
 const Link = ({
   x, y,
@@ -17,27 +16,27 @@ const Link = ({
     <circle
       cx={x}
       cy={y}
-      r={pinRadius - 2}
+      r={strokeWidth}
     />
     <line
       x1={x} y1={y} x2={x2} y2={y2}
       stroke={stroke}
-      strokeDasharray={selected ? strokeDasharray : undefined}
       strokeWidth={selected ? highlightedStrokeWidth : strokeWidth}
     />
     <circle
       cx={x2}
       cy={y2}
-      r={pinRadius - 2}
+      r={strokeWidth}
     />
   </g>
 )
 
 Link.propTypes = {
-  x: PropTypes.number,
-  y: PropTypes.number,
-  x2: PropTypes.number,
-  y2: PropTypes.number
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  x2: PropTypes.number.isRequired,
+  y2: PropTypes.number.isRequired,
+  selectLink: PropTypes.func.isRequired
 }
 
 export default Link

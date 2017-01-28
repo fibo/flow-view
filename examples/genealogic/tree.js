18e2f2678d280615fda0a35fe09ea17ff60873ba
fibo <casati_gianluca@yahoo.it>
2017-01-28 16:32:47 +0100

    Homer and Marge

--- examples/genealogic/tree.js
+++ examples/genealogic/tree.js
@@ -1,6 +1,16 @@
 import { Canvas } from 'flow-view'
 
 const canvas = new Canvas('drawing', {
+  node: {
+    dad: {
+      x: 10, y: 10,
+      text: 'Homer'
+    },
+    mom: {
+      x: 60, y: 20,
+      text: 'Marge'
+    }
+  },
   nodeList: [
     'Homer',
     'Marge',

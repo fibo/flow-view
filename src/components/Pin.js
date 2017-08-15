65b220eef23694ce80b941009d28cf59e7e5d147
fibo <casati_gianluca@yahoo.it>
2017-08-15 16:03:46 +0200

    added Pin components

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
+import bindme from 'bindme'
+
 import {
   Color,
+  Id,
+  NodeIdAndPosition,
   Point
 } from './types'
 
 export default class Pin extends React.Component {
   props: Point & {
     color: Color,
+    createLink: ?({ from: NodeIdAndPosition, to?: NodeIdAndPosition }) => Id,
+    nodeIdAndPosition: NodeIdAndPosition,
     size: number
   }
+
+  static defaultProps = {
+    onMouseDown: Function.prototype,
+    onMouseUp: Function.prototype
+  }
+
+  constructor () {
+    bindme(super(),
+      'onMouseDown',
+      'onMouseUp'
+    )
+  }
+
+  onMouseDown () { /* to be overridden */ }
+
+  onMouseUp () { /* to be overridden */ }
 
   render () {
     const {
@@ -20,12 +42,14 @@ export default class Pin extends React.Component {
     } = this.props
 
     return (
-      <rect>
+      <rect
         fill={color}
         height={size}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
         transform={`translate(${x},${y})`}
         width={size}
-      </rect>
+      />
     )
   }
 }

e878392db3574fc6fdaffba645542378ce27782e
fibo <casati_gianluca@yahoo.it>
2017-11-10 22:07:21 +0100

    changed default theme colors

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,30 +1,13 @@
 // @flow
 
-/*
- * Palette from https://www.materialpalette.com/lime/grey
- *
- * Thanks to Tania and Lucilla.
- *
- * dark primary color #AFB42B
- * primary color #CDDC39
- * light primary color #F0F4C3
- *
- * accent color #9E9E9E
- * primary text #212121
- * secondary text #757575
- *
- * text / icons #212121
- * divider color #BDBDBD
- */
-
 import type { Color } from './types'
 
 export const defaultTheme = {
   fontFamily: 'Courier',
   fontSize: 17,
   frameBorder: '1px solid #F0F4C3',
-  darkPrimaryColor: '#AFB42B',
-  primaryColor: '#CDDC39',
+  darkPrimaryColor: '#52BE80',
+  primaryColor: '#A9DFBF',
   lineWidth: 3,
   nodeBarColor: '#BDBDBD',
   nodeBodyColor: '#FFFFFF',

a28fa3a1b747ec28fd255a32a64dc1ba97d8ff79
fibo <casati_gianluca@yahoo.it>
2018-01-21 18:33:43 +0200

    more types

--- src/components/CrossButton.js
+++ src/components/CrossButton.js
@@ -1,13 +1,20 @@
 import NodeButton from './NodeButton'
 
+const ray = (size: number): number => {
+  return size * Math.sqrt(0.47)
+}
+
+const shape = (size: number): string => {
+  const u = size / 6 // unit
+
+  return `M ${4 * u} ${3 * u} L ${6 * u} ${u} L ${5 * u} 0 L ${3 * u} ${2 * u} L ${u} 0 L 0 ${u} L ${2 * u} ${3 * u} L 0 ${5 * u} L ${u} ${6 * u} L ${3 * u} ${4 * u} L ${5 * u} ${6 * u} L ${6 * u} ${5 * u} Z`
+}
+
 export default class CrossButton extends NodeButton {
-  ray (): number {
-    return this.props.size * Math.sqrt(0.47)
-  }
-
-  shape (size: number): string {
-    const u = size / 6 // unit
-
-    return `M ${4 * u} ${3 * u} L ${6 * u} ${u} L ${5 * u} 0 L ${3 * u} ${2 * u} L ${u} 0 L 0 ${u} L ${2 * u} ${3 * u} L 0 ${5 * u} L ${u} ${6 * u} L ${3 * u} ${4 * u} L ${5 * u} ${6 * u} L ${6 * u} ${5 * u} Z`
+  static defaultProps = {
+    action: Function.prototype,
+    disabled: false,
+    ray,
+    shape
   }
 }
--- test/jsonSchema.test.js
+++ test/jsonSchema.test.js
@@ -1,10 +1,10 @@
+const validator = require('is-my-json-valid')
+
+const schema = require('../docs/schema.json')
+const sampleView = require('../examples/render/sample-view.json')
+
+const validate = validator(schema)
+
 test('JSON schema', () => {
-  const validator = require('is-my-json-valid')
-
-  const schema = require('../docs/schema.json')
-  const sampleView = require('../examples/render/sample-view.json')
-
-  const validate = validator(schema)
-
   expect(validate(sampleView)).toBeTruthy()
 })

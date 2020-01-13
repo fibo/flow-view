01d01e25b8b1020711002c8f51ae763554c23605
fibo <casati_gianluca@yahoo.it>
2020-01-13 15:57:52 +0100

    use rollup

---
+++ examples/basic/usage.js
@@ -0,0 +1,43 @@
+import { FlowViewCanvas } from '../../flow-view.min.js'
+
+const graph = {
+  nodes: [
+    {
+      id: 'a',
+      x: 80,
+      y: 100,
+      text: 'Drag me',
+      outs: [
+        { id: 'out1' },
+        { id: 'out2' },
+        { id: 'out3' }
+      ]
+    },
+    {
+      id: 'b',
+      x: 180,
+      y: 200,
+      text: 'Click me',
+      ins: [
+        { id: 'in1' },
+        { id: 'in2' }
+      ],
+      outs: [
+        { id: 'out4' }
+      ]
+    }
+  ],
+  links: [
+    {
+      id: 'c',
+      from: 'out1',
+      to: 'in1'
+    }
+  ]
+}
+
+const container = document.getElementById('drawing')
+
+const canvas = new FlowViewCanvas(container)
+
+canvas.loadGraph(graph)

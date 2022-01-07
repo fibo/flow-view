2229c711957ca9310780247e01a46a9af67f2780
fibo <casati_gianluca@yahoo.it>
2022-01-07 01:43:41 +0100

    show autocompletion

--- items/selector.js
+++ items/selector.js
@@ -36,12 +36,12 @@ export class FlowViewSelector extends FlowViewBase {
 
     element.setAttribute("tabindex", 0);
 
-    const input = this.input = document.createElement("input");
-    element.appendChild(input);
-
     const hint = this.hint = document.createElement("input");
     hint.classList.add(`${FlowViewSelector.cssClassName}__hint`);
     element.appendChild(hint);
+
+    const input = this.input = document.createElement("input");
+    element.appendChild(input);
 
     this.position = position;
 
@@ -53,29 +53,51 @@ export class FlowViewSelector extends FlowViewBase {
 
     this._onDblclick = this.onDblclick.bind(this);
     element.addEventListener("dblclick", this._onDblclick);
-    this._onKeydown = this.onKeydown.bind(this);
-    element.addEventListener("keydown", this._onKeydown);
     this._onPointerdown = this.onPointerdown.bind(this);
     element.addEventListener("pointerdown", this._onPointerdown);
+
+    this._onKeydown = this.onKeydown.bind(this);
+    input.addEventListener("keydown", this._onKeydown);
+    this._onKeyup = this.onKeyup.bind(this);
+    input.addEventListener("keyup", this._onKeyup);
   }
 
   dispose() {
-    const { element } = this;
+    const { element, input } = this;
+
     element.removeEventListener("dblclick", this._onDblclick);
-    element.removeEventListener("keydown", this._onKeydown);
     element.removeEventListener("pointerdown", this._onPointerdown);
+
+    input.removeEventListener("keydown", this._onKeydown);
+    input.removeEventListener("keyup", this._onKeyup);
   }
 
   focus() {
     this.input.focus();
   }
 
-  get suggestion() {
+  get completion() {
     return this.hint.getAttribute("placeholder");
   }
 
-  set suggestion(text) {
+  set completion(text) {
     this.hint.setAttribute("placeholder", text);
+  }
+
+  get matchingNodeDefinitions() {
+    const { input: { value } } = this;
+
+    // Type at least few chars to start showing completion.
+    if (value.length < 2) return [];
+
+    return this.nodeDefinitions.filter(({ type }) => (
+      // input value fits into node type...
+      type.startsWith(value) &&
+      // ...but they are not the same yet.
+      // Otherwise if a type starts with another type,
+      // some completions could be missed.
+      type !== value
+    ));
   }
 
   set position({ x, y }) {
@@ -108,20 +130,62 @@ export class FlowViewSelector extends FlowViewBase {
         this.view.removeSelector();
         break;
       }
+
       case event.code === "Escape": {
         this.view.removeSelector();
         break;
       }
+
       case event.code === "Tab": {
         event.preventDefault();
-        const { suggestion } = this;
-        if (suggestion) {
-          this.input.value = suggestion;
+        const { completion } = this;
+        if (completion) {
+          this.input.value = completion;
         }
         break;
       }
+
       default: {
         // console.log(event.code);
+      }
+    }
+  }
+
+  onKeyup(event) {
+    event.stopPropagation();
+
+    const { input: { value }, matchingNodeDefinitions } = this;
+
+    switch (matchingNodeDefinitions.length) {
+      case 0: {
+        this.completion = "";
+        break;
+      }
+      case 1: {
+        this.completion = matchingNodeDefinitions[0].type;
+        break;
+      }
+      default: {
+        let completion = value;
+
+        const shortestMatch = matchingNodeDefinitions.reduce((
+          shortest,
+          match,
+        ) => (shortest.type.length < match.type.length ? shortest : match));
+
+        for (let i = value.length; i < shortestMatch.type.length; i++) {
+          const currentChar = shortestMatch.type[i];
+
+          if (
+            matchingNodeDefinitions.every(({ type }) =>
+              type.startsWith(completion + currentChar)
+            )
+          ) {
+            completion += currentChar;
+          }
+        }
+
+        this.completion = completion;
       }
     }
   }
